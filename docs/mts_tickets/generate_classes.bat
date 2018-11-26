9fe208af8f81f78c9470a37c020f003c32989993
d.hrovat <d.hrovat@sportradar.com>
2018-11-26 13:05:41 +0100

    Changed ticket schema to version 2.2

--- docs/mts_tickets/generate_classes.bat
+++ docs/mts_tickets/generate_classes.bat
@@ -1,16 +1,16 @@
 echo off
 cls
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.Ticket /input:ticket-2.1-schema.json /output:Ticket\TicketDTO.cs
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketAckDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketAck /input:ticket-ack-2.0-schema.json /output:TicketAck\TicketAckDTO.cs
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCancelDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCancel /input:ticket-cancel-2.0-schema.json /output:TicketCancel\TicketCancelDTO.cs
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCancelAckDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCancelAck /input:ticket-cancel-ack-2.0-schema.json /output:TicketCancelAck\TicketCancelAckDTO.cs
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketResponseDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketResponse /input:ticket-response-2.1-schema.json /output:TicketResponse\TicketResponseDTO.cs
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCancelResponseDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCancelResponse /input:ticket-cancel-response-2.0-schema.json /output:TicketCancelResponse\TicketCancelResponseDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.Ticket /input:ticket-schema.json /output:Ticket\TicketDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketAckDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketAck /input:ticket-ack-schema.json /output:TicketAck\TicketAckDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCancelDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCancel /input:ticket-cancel-schema.json /output:TicketCancel\TicketCancelDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCancelAckDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCancelAck /input:ticket-cancel-ack-schema.json /output:TicketCancelAck\TicketCancelAckDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketResponseDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketResponse /input:ticket-response-schema.json /output:TicketResponse\TicketResponseDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCancelResponseDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCancelResponse /input:ticket-cancel-response-schema.json /output:TicketCancelResponse\TicketCancelResponseDTO.cs
 
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketReofferCancelDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketReofferCancel /input:reoffer-cancel-2.0-schema.json /output:TicketReofferCancel\TicketReofferCancelDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketReofferCancelDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketReofferCancel /input:ticket-reoffer-cancel-schema.json /output:TicketReofferCancel\TicketReofferCancelDTO.cs
 
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCashoutDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCashout /input:ticket-cashout-2.0-schema.json /output:TicketCashout\TicketCashoutDTO.cs
-..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCashoutResponseDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCashoutResponse /input:ticket-cashout-response-2.0-schema.json /output:TicketCashoutResponse\TicketCashoutResponseDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCashoutDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCashout /input:ticket-cashout-schema.json /output:TicketCashout\TicketCashoutDTO.cs
+..\..\build\tools\nswag\nswag.exe jsonschema2csclient /arraytype:IEnumerable /name:TicketCashoutResponseDTO /namespace:Sportradar.MTS.SDK.Entities.Internal.Dto.TicketCashoutResponse /input:ticket-cashout-response-schema.json /output:TicketCashoutResponse\TicketCashoutResponseDTO.cs
 
 echo.
 echo Replacing 'double' to 'long' ...
